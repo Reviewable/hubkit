fc0b3d757e9d414c58e04de279abc445e54a26dc
Sebastian Noack <sebastian.noack@gmail.com>
2025-04-30 19:24:18 -0400

    Add #field directive to include fields based on GraphQL schema

--- hubkit.js
+++ hubkit.js
@@ -510,40 +510,64 @@ if (typeof require !== 'undefined') {
     return Promise.resolve(
       fullOptions.onRequest && fullOptions.onRequest(fullOptions)
     ).then(function() {
+      var asyncSubstitutions = [];
       query = query.replace(
-        /#(\w+)\s*\(([^)]+)\)\s*\{([\s\S]*?)#\}/g,
+        /#(\w+)\s*\(([^)]+)\)(?:\s*\{([\s\S]*?)#\})?/g,
         function(match, directive, arg, contents) {
+          var requiresBody = true;
+          var substitution = '';
           switch (directive) {
             case 'ghe':
-              if (satisfiesGheVersion(fullOptions, arg)) return contents;
               if (!fullOptions.gheVersion) {
                 throw new Error('Hubkit unable to process #ghe directive: gheVersion missing');
               }
-              return '';
+              if (satisfiesGheVersion(fullOptions, arg)) substitution = contents;
+              break;
             case 'scope':
               if (!fullOptions.scopes) {
                 throw new Error('Hubkit unable to process #scope directive: scopes missing');
               }
-              return fullOptions.scopes.includes(arg) ? contents : '';
+              if (fullOptions.scopes.includes(arg)) substitution = contents;
+              break;
+            case 'field':
+              var args = arg.split(',').map(function(s) {return s.trim();});
+              if (args.length < 2) {
+                throw new Error(
+                  'Hubkit unable to process #field directive: expected at least 2 arguments');
+              }
+              requiresBody = false;
+              substitution = '#async(' + match.index + ')';
+              asyncSubstitutions.push(pickSupportedField(self, args[0], args.slice(1)).then(
+                function(field) {
+                  query = query.replace(substitution,
+                    field ? contents === undefined ? field : contents : '');
+                }));
+              break;
             default:
               throw new Error('Unknown Hubkit GraphQL preprocessing directive: #' + directive);
           }
+          if (requiresBody && contents === undefined) {
+            throw new Error('Hubkit unable to process #' + directive + ' directive: missing body');
+          }
+          return substitution;
         }
       );
-      if (/#(\w+)\s*\(([^)]+)\)/.test(query)) {
-        throw new Error(
-          'Hubkit preprocessing directives may not have been correctly terminated: ' + query);
-      }
-      var postOptions = defaults({body: {query: query}}, options);
-      delete postOptions.onRequest;
-      postOptions.host =
-        options.graphHost || options.host || self.defaultOptions.graphHost ||
-        self.defaultOptions.host;
-      if (options.variables) {
-        postOptions.body.variables = options.variables;
-        delete postOptions.variables;
-      }
-      return self.request('POST /graphql', postOptions);
+      return Promise.all(asyncSubstitutions).then(function() {
+        if (/#(\w+)\s*\(([^)]+)\)/.test(query)) {
+          throw new Error(
+            'Hubkit preprocessing directives may not have been correctly terminated: ' + query);
+        }
+        var postOptions = defaults({body: {query: query}}, options);
+        delete postOptions.onRequest;
+        postOptions.host =
+          options.graphHost || options.host || self.defaultOptions.graphHost ||
+          self.defaultOptions.host;
+        if (options.variables) {
+          postOptions.body.variables = options.variables;
+          delete postOptions.variables;
+        }
+        return self.request('POST /graphql', postOptions);
+      });
     });
   };
 
@@ -702,6 +726,30 @@ if (typeof require !== 'undefined') {
     var actualVersion = options.gheVersion.split('.').map(function(x) {return parseInt(x, 10);});
     return actualVersion[0] > neededVersion[0] ||
       actualVersion[0] === neededVersion[0] && actualVersion[1] >= neededVersion[1];
+  }
+
+  var schemaCache = {};
+  function pickSupportedField(self, type, fields) {
+    var fieldsPromise;
+    if (Object.prototype.hasOwnProperty.call(schemaCache, type)) {
+      fieldsPromise = schemaCache[type];
+    } else {
+      fieldsPromise = schemaCache[type] = self.graph(
+        'query (type: String!) { __type(name: $type) { fields { name } } }',
+        {variables: {type: type}}
+      ).then(function(result) {
+        if (!result.__type) {
+          throw new Error(
+            'Hubkit unable to process #field directive: Type ' + type + ' does not exist.');
+        }
+        return result.__type.fields.map(function(field) {return field.name;});
+      });
+    }
+    return fieldsPromise.then(function(schemaFields) {
+      for (var i = 0; i < fields.length; i++) {
+        if (schemaFields.includes(fields[i])) return fields[i];
+      }
+    });
   }
 
   return Hubkit;
